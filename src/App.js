ad536139504b402fb37341498f61561d6a907087
Armando García <armandobfmv@gmail.com>
2021-10-27 10:56:34 -0500

    First steps with JSX

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let nombre = "Armando";
+  let auth = false;
+  let seasons = ["spring", "summer", "fall", "winter"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Test</label>
+        <input type="text" id="nombre" />
+        <h1>Hola {nombre}</h1>
+        <p>
+          {auth ? "El usuario esta logeado" : "El usuario debe iniciar sesión"}
+        </p>
+        <ul>
+          {seasons.map((el, index) => (
+            <li key={index}>{el}</li>
+          ))}
+        </ul>
+        <p>{2 + 1}</p>
         <p>
           Edit <code> src / App.js </code> and save to reload.
         </p>
