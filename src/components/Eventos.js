ac90c5daca0308719396fa3b5a8f2f865e5c9765
Armando García <armandobfmv@gmail.com>
2021-10-28 19:42:11 -0500

    Events and property initializers in class components

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Eventos extends Component {
+export class EventosES6 extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,12 +12,14 @@ export default class Eventos extends Component {
 
   sumar(e) {
     console.log("Sumando");
+    console.log(this);
     this.setState({
       contador: this.state.contador + 1,
     });
   }
   restar(e) {
     console.log("Restando");
+    console.log(this);
     this.setState({
       contador: this.state.contador - 1,
     });
@@ -25,12 +27,47 @@ export default class Eventos extends Component {
   render() {
     return (
       <div>
-        <h2>Events in class Components</h2>
+        <h2> Events in class Components ES6</h2>
         <nav>
-          <button onClick={this.sumar}>+</button>
-          <button onClick={this.restar}>-</button>
+          <button onClick={this.sumar}> + </button>
+          <button onClick={this.restar}> - </button>
         </nav>
-        <h3>{this.state.contador}</h3>
+        <h3> {this.state.contador} </h3>
+      </div>
+    );
+  }
+}
+export class EventosES7 extends Component {
+  state = {
+    contador: 0,
+  };
+
+  //Use arrow functions instead normal functions
+
+  sumar = e => {
+    console.log("Sumando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  };
+  restar = e => {
+    console.log("Restando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2> Events in class Components ES7+ </h2>
+        <nav>
+          <button onClick={this.sumar}> + </button>
+          <button onClick={this.restar}> - </button>
+        </nav>
+        <h3> {this.state.contador} </h3>
       </div>
     );
   }
