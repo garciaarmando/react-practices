643b4035bd30eb39f4a347210905b0f9e47ab4ae
Armando García <armandobfmv@gmail.com>
2021-11-25 16:25:48 -0600

    Components Link and NavLink

--- 02-react-router/src/components/BasicsConcepts.js
+++ 02-react-router/src/components/BasicsConcepts.js
@@ -3,9 +3,30 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
+import Error404 from "../pages/Error404";
 import Home from "../pages/Home";
+import ConceptsMenu from "./ConceptsMenu";
 
 const BasicsConcepts = () => {
+  return (
+    <div>
+      <h2>Basics Concepts</h2>
+      <Router>
+        <ConceptsMenu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default BasicsConcepts;
+
+/* const BasicsConcepts = () => {
   return (
     <Router>
       <Routes>
@@ -24,3 +45,4 @@ const BasicsConcepts = () => {
 };
 
 export default BasicsConcepts;
+ */
---
+++ 02-react-router/src/components/ConceptsMenu.js
@@ -0,0 +1,50 @@
+import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
+
+const ConceptsMenu = () => {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <span>
+            HTML links (not recommended due the entire application re-render)
+          </span>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <span>Link Component (without re-render)</span>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="contact">Contact</Link>
+          <Link to="/no-place">Error 404</Link>
+        </li>
+        <li>
+          <span>NavLink Component</span>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? " activated" : "")}>
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? " activated" : "")}>
+            About
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? "activated" : "")}>
+            Contact
+          </NavLink>
+          <NavLink
+            to="/no-place"
+            className={({ isActive }) => (isActive ? "activated" : "")}>
+            Error 404
+          </NavLink>
+        </li>
+      </ol>
+    </nav>
+  );
+};
+
+export default ConceptsMenu;
