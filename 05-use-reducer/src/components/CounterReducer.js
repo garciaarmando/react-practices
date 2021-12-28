4e2e9a535d80b9270e3fe15239a7475e63cf7659
Armando García <armandobfmv@gmail.com>
2021-12-27 20:59:40 -0600

    using the concept of payload on useReducer

--- 05-use-reducer/src/components/CounterReducer.js
+++ 05-use-reducer/src/components/CounterReducer.js
@@ -2,12 +2,27 @@ import { useReducer } from "react";
 
 const initialState = { contador: 0 };
 
+const TYPES = {
+  INCREMENT_5: "INCREMENT_5",
+  INCREMENT: "INCREMENT",
+  RESET: "RESET",
+  DECREMENT: "DECREMENT",
+  DECREMENT_5: "DECREMENT_5",
+};
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case TYPES.INCREMENT_5:
+      return { contador: state.contador + action.payload };
+    case TYPES.INCREMENT:
       return { contador: state.contador + 1 };
-    case "DECREMENT":
+    case TYPES.RESET:
+      return initialState;
+    case TYPES.DECREMENT_5:
+      return { contador: state.contador - action.payload };
+    case TYPES.DECREMENT:
       return { contador: state.contador - 1 };
+
     default:
       return state;
   }
@@ -17,13 +32,24 @@ const CounterReducer = () => {
   // const [contador, setContador] = useState(0);
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const sumar = () => dispatch({ type: "INCREMENT" });
-  const restar = () => dispatch({ type: "DECREMENT" });
+  const sumar5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
+
+  const sumar = () => dispatch({ type: TYPES.INCREMENT });
+
+  const reset = () => dispatch({ type: TYPES.RESET });
+
+  const restar = () => dispatch({ type: TYPES.DECREMENT });
+
+  const restar5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+
   return (
     <div style={{ textAlign: "center" }}>
       <h3>Counter with useReducer</h3>
+      <button onClick={sumar5}>+5</button>
       <button onClick={sumar}>+</button>
+      <button onClick={reset}>0</button>
       <button onClick={restar}>-</button>
+      <button onClick={restar5}>-5</button>
       <p>{state.contador}</p>
     </div>
   );
